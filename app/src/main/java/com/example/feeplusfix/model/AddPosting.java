a79fdb48494e7037a60174d1a0b3923ec1683f0a
nashruddinputra <84800968+nashruddinputra@users.noreply.github.com>
2021-08-18 00:10:21 +0700

    detail posts

--- app/src/main/java/com/example/feeplusfix/model/AddPosting.java
+++ app/src/main/java/com/example/feeplusfix/model/AddPosting.java
@@ -5,16 +5,22 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -29,6 +35,10 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
 public class AddPosting extends AppCompatActivity {
@@ -38,13 +48,15 @@ public class AddPosting extends AppCompatActivity {
     private ImageView imgViewPhoto;
     private int CAMERA_PERMISSION_CODE = 101;
     private int CAMERA_REQUEST_CODE = 102;
-    private int GALERY_REQUEST_CODE= 105;
+    private int GALLERY_REQUEST_CODE = 105;
+    private String currentPhotoPath;
 
     FirebaseDatabase mDatabase;
     FirebaseAuth fAuth;
     FirebaseUser fUser;
 
     String userId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,6 +80,8 @@ public class AddPosting extends AppCompatActivity {
         btnOpenGalery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(gallery, GALLERY_REQUEST_CODE);
 
             }
         });
@@ -88,20 +102,20 @@ public class AddPosting extends AppCompatActivity {
                 String hargabarang = etHargaBarang.getText().toString().trim();
 
                 boolean isvalid = true;
-                if(TextUtils.isEmpty(namabarang)){
+                if (TextUtils.isEmpty(namabarang)) {
                     etNamaBarang.setError("Masukan Nama Barang!");
                     isvalid = false;
                 }
-                if(TextUtils.isEmpty(deskripsibarang)){
+                if (TextUtils.isEmpty(deskripsibarang)) {
                     etDeskripsiBarang.setError("Masukan Deskripsi Barang!");
                     isvalid = false;
                 }
-                if(TextUtils.isEmpty(hargabarang)){
+                if (TextUtils.isEmpty(hargabarang)) {
                     etHargaBarang.setError("Masukan Harga Barang!");
                     isvalid = false;
                 }
-                if(!isvalid){
-                    Toast.makeText(AddPosting.this,"Posting Barang Gagal!", Toast.LENGTH_SHORT).show();
+                if (!isvalid) {
+                    Toast.makeText(AddPosting.this, "Posting Barang Gagal!", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
@@ -127,35 +141,37 @@ public class AddPosting extends AppCompatActivity {
     }
 
     public void writeNewPost(String namaBarang, String deskripsiBarang, String hargaBarang) {
-        Post tambahbarang = new Post(namaBarang,deskripsiBarang,hargaBarang);
+        Post tambahbarang = new Post(namaBarang, deskripsiBarang, hargaBarang);
 
         DatabaseReference dataref = mDatabase.getReference().child("posts").child(userId).child(this.getSaltString());
 
         dataref.setValue(tambahbarang)
-            .addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Toast.makeText(AddPosting.this,"Posting Barang Berhasil", Toast.LENGTH_SHORT).show();
-                Intent i = new Intent (AddPosting.this, MainActivity.class);
-                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(i);
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(AddPosting.this, "Posting Barang Berhasil", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(AddPosting.this, MainActivity.class);
+                        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(i);
 
-                }
-            })
-            .addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(AddPosting.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(AddPosting.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
 
     }
 
-    private void AskPermissionsCamera(){
-        if(ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
-        }else{
+    private void AskPermissionsCamera() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+
+        } else {
             OpenCamera();
+            dispatchTakePictureIntent();
         }
     }
 
@@ -171,17 +187,70 @@ public class AddPosting extends AppCompatActivity {
         }
     }
 
-    private void OpenCamera(){
+    private void OpenCamera() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent,CAMERA_REQUEST_CODE);
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == CAMERA_REQUEST_CODE){
-            Bitmap image = (Bitmap) data .getExtras().get("data");
-            imgViewPhoto.setImageBitmap(image);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                File f = new File(currentPhotoPath);
+                imgViewPhoto.setImageURI(Uri.fromFile(f));
+
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                Uri contentUri = Uri.fromFile(f);
+                mediaScanIntent.setData(contentUri);
+                this.sendBroadcast(mediaScanIntent);
+            }
+        }
+        if(requestCode == GALLERY_REQUEST_CODE){
+            if(resultCode == Activity.RESULT_OK){
+                Uri contentUri = data.getData();
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "JPEG_" + timeStamp +"."+getFileExt(contentUri);
+                imgViewPhoto.setImageURI(contentUri);
+            }
+
         }
     }
+
+    private String getFileExt(Uri contentUri) {
+        ContentResolver c = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(c.getType(contentUri));
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName, ".jpg", storageDir);
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.feeplusfix",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
+
 }
